b2e745881e6caa2ec52464c9e016b91859a3f3c9
Hillary-lab <hillarysherien.bangcoyoiii@evsu.edu.ph>
2021-06-10 12:04:13 +0700

    activity 3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController, NavController} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,48 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  contName ="";
+  contNumber = "";
+  contacts: any = [];
+
+  constructor(public alertCtrl: AlertController) { }
+
+  save() {
+    let contact = {
+      name: this.contName,
+      number: this.contNumber
+    }
+      this.contacts.push(contact);
+      this.clearField();
+    }
+
+    clearField() {
+      this.contName ="";
+      this.contNumber = "";
+    }
+
+    async confirm(con){
+      const confirm = await this.alertCtrl.create({
+        message: 'Do you want to delete this contact?',
+        buttons: [
+          {
+            text: 'CANCEL',
+            role: 'cancel',
+            handler: () => {
+              }
+            },
+            {
+              text: 'Confirm',
+              handler: () => {
+               let index = this.contacts.indexOf(con);
+               if(index > -1){
+                 this.contacts.splice(index, 1);
+               }
+              }
+            }
+          ]
+        });
+      await confirm.present();
+    }
 
 }
